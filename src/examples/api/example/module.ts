20dd7933d3e926157d5c649836f95d62391fad47
smithjke <smithjke@gmail.com>
2022-12-05 22:15:08 +0300

    BREAKING CHANGE: test up major version

--- src/examples/api/example/module.ts
+++ src/examples/api/example/module.ts
@@ -2,6 +2,8 @@ import { AppModule } from '../../../app';
 import { migration } from './migration';
 import { plugin } from './plugin';
 
+// test up major version
+
 const module = new AppModule('example', {
   migration,
   plugin,
