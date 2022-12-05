93a5d335269cb81d59911d6eeae06fa13841c91c
smithjke <smithjke@gmail.com>
2022-12-05 22:18:05 +0300

    perf: second version up

--- src/examples/api/example/module.ts
+++ src/examples/api/example/module.ts
@@ -2,7 +2,7 @@ import { AppModule } from '../../../app';
 import { migration } from './migration';
 import { plugin } from './plugin';
 
-// test up major version
+// test up major version 2
 
 const module = new AppModule('example', {
   migration,
